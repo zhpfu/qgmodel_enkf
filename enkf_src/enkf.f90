bf002badde5f8f901e74dce6327af7cc1494648c
Yue Ying <hardyying@gmail.com>
2017-08-30 10:25:37 -0600

    first working enkf code!

--- enkf_src/enkf.f90
+++ enkf_src/enkf.f90
@@ -5,7 +5,7 @@ program enkf
   use fft
   use scales
   use localization
-  use relaxation
+  use inflation
   use obs
   use grid
 
@@ -18,21 +18,24 @@ program enkf
   real :: innov,varb,varo,norm,alpha
   complex,allocatable,dimension(:,:,:) :: uspec
   complex(C_DOUBLE_COMPLEX),allocatable,dimension(:,:) :: spec,hspec
-  integer,allocatable,dimension(:,:) :: kx,ky
-  integer,allocatable,dimension(:,:,:) :: x,y,z
+  real,allocatable,dimension(:,:) :: kx,ky
+  real,allocatable,dimension(:,:,:) :: x,y,z
   real,allocatable,dimension(:) :: hu,yo,obsv
-  real,allocatable,dimension(:,:,:) :: um,uobs,sig,sigf,dist,cova,loc,ysave
-  real,allocatable,dimension(:,:,:,:) :: u,umsave,um_ms
-  real,allocatable,dimension(:,:,:,:,:) :: u_ms,usave,usavev
+  real,allocatable,dimension(:,:) :: ya,yb
+  real,allocatable,dimension(:,:,:) :: um,umf,sig,sigf,dist,cova,loc
+  real,allocatable,dimension(:,:,:,:) :: u,uf,uobs,um_ms
+  real,allocatable,dimension(:,:,:,:,:) :: u_ms
 
-  integer :: kmax,nz,nens,localize_opt,ob_thin,find_roi,relax_opt
-  real :: ob_err,obs_val,relax_coef
-  logical :: debug,relax_adapt
+  integer :: kmax,nz,nens,localize_opt,ob_thin,find_roi,relax_opt,state_type
+  real :: ob_err,obs_val,relax_coef,inflate_coef
+  logical :: debug,relax_adapt,inflate_adapt,use_aoei
   integer,dimension(50) :: krange,localize_cutoff,ob_type
   integer,allocatable,dimension(:) :: kr,roi,obt
 
   namelist/enkf_param/kmax,nz,nens,localize_opt,localize_cutoff,krange,find_roi,&
-                      ob_thin,ob_err,ob_type,relax_opt,relax_adapt,relax_coef,debug
+                      ob_thin,ob_err,ob_type,state_type, &
+                      inflate_adapt,inflate_coef,use_aoei, &
+                      relax_opt,relax_adapt,relax_coef,debug
   
   !initialize
   call parallel_start()
@@ -76,13 +79,17 @@ program enkf
   allocate(uspec(nkx,nky,nz))
   allocate(spec(nx,ny),hspec(nkx,nky))
   allocate(u(nx,ny,nz,nm),um(nx,ny,nz))
-  allocate(usave(nx,ny,nz,nm,2),umsave(nx,ny,nz,2))
+  allocate(uf(nx,ny,nz,nm),umf(nx,ny,nz))
   allocate(sig(nx,ny,nz),sigf(nx,ny,nz))
-  allocate(uobs(nx,ny,nz))
+  allocate(uobs(nx,ny,nz,nm))
   allocate(u_ms(nx,ny,nz,ns,nm),um_ms(nx,ny,nz,ns))
   allocate(hu(nens+1))
 
-  call grid3d((/(i,i=1,nx)/),(/(i,i=1,ny)/),(/(i,i=1,nz)/),x,y,z)
+  call grid3d(real((/(i,i=1,nx)/)),real((/(i,i=1,ny)/)),real((/(i,i=1,nz)/)),x,y,z)
+
+  call grid2d(real((/(i,i=-kmax,kmax)/)),real((/(j,j=0,kmax)/)),kx,ky)
+  where(kx==0) kx=1
+  where(ky==0) ky=1
 
   !read in observation file
   write(obfile,'(a,a,i5.5)') trim(obsdir),'/',nt
@@ -96,7 +103,18 @@ program enkf
       write(infile,'(a,a,i4.4,a,i5.5,a)') trim(workdir),'/',ie,'/f_',nt,'.bin'
       call read_field(infile,nkx,nky,nz,uspec)
       do k=1,nz
-        spec=fullspec(dcmplx(uspec(:,:,k)))
+        hspec=dcmplx(uspec(:,:,k))
+        select case (state_type) !convert psi to state variable
+          case (1) !u
+            hspec=hspec*dcmplx(0.0,-ky)
+          case (2) !v
+            hspec=hspec*dcmplx(0.0,kx)
+          case (4) !zeta
+            hspec=hspec*dcmplx(-(kx**2+ky**2),0.0)
+          case (5) !temp
+            hspec=hspec*dcmplx(-sqrt(kx**2+ky**2),0.0)
+        end select
+        spec=fullspec(hspec)
         spec=ifftshift(spec)
         u(:,:,k,m)=real(ifft2(spec))
       end do
@@ -111,26 +129,103 @@ program enkf
     if(ie<=nens) &
       u(:,:,:,m)=u(:,:,:,m)-um
   end do
-  usave(:,:,:,:,1)=u; umsave(:,:,:,1)=um !prior perturbation and mean
+  uf=u; umf=um !prior perturbation and mean
+
+  nobs=ceiling(real(nx)/ob_thin)*ceiling(real(ny)/ob_thin)*1*nv
+
+  !prior statistics in obs space
+  allocate(obsv(nrec),yo(nobs),ya(nobs,nens+1),yb(nobs,nens+1))
+  n=0
+  do v=1,nv
+    select case (obt(v))
+      case (1) !u
+        obsv=obs_u
+      case (2) !v
+        obsv=obs_v
+      case (3) !psi 
+        obsv=obs_psi
+      case (4) !zeta 
+        obsv=obs_zeta
+      case (5) !temp
+        obsv=obs_temp
+    end select
+    do m=1,nm
+      ie=(m-1)*nprocs+proc_id+1
+      if(ie<=nens) then
+        uobs(:,:,:,m)=uf(:,:,:,m)+umf
+        do k=1,nz
+          spec=dcmplx(uobs(:,:,k,m),0.0)
+          spec=fft2(spec)/(nx*ny)
+          spec=fftshift(spec)
+          hspec=halfspec(spec)
+          select case (state_type)
+            case (1) !u
+              hspec=hspec/dcmplx(0.0,-ky)
+            case (2) !v
+              hspec=hspec/dcmplx(0.0,kx)
+            case (4) !zeta
+              hspec=hspec/dcmplx(-(kx**2+ky**2),0.0)
+            case (5) !temp
+              hspec=hspec/dcmplx(-sqrt(kx**2+ky**2),0.0)
+          end select
+          select case (obt(v))
+            case (1)
+              hspec=hspec*dcmplx(0.0,-ky)
+            case (2)
+              hspec=hspec*dcmplx(0.0,kx)
+            case (4)
+              hspec=hspec*dcmplx(-(kx**2+ky**2),0.0)
+            case (5)
+              hspec=hspec*dcmplx(-sqrt(kx**2+ky**2),0.0)
+          end select
+          spec=fullspec(hspec)
+          spec=ifftshift(spec)
+          uobs(:,:,k,m)=real(ifft2(spec))
+        end do
+      end if
+    end do
+
+    do k=1,1 
+    do j=1,ny,ob_thin
+    do i=1,nx,ob_thin
+      n=n+1
+      p=(k-1)*nx*ny+(j-1)*ny+i
+      yo(n)=obsv(p)
+      do m=1,nm
+        ie=(m-1)*nprocs+proc_id+1
+        if(ie<=nens) &
+          call obs_interp(x,y,z,uobs(:,:,:,m),obs_x(p),obs_y(p),obs_z(p),yb(n,ie))
+      end do
+    end do
+    end do
+    end do
+  end do
+  call MPI_Allreduce(yb,yb,nobs*(nens+1),MPI_REAL8,MPI_SUM,comm,ierr)
+  yb(:,nens+1)=sum(yb(:,1:nens),2)/real(nens)
+  do m=1,nens
+    yb(:,m)=yb(:,m)-yb(:,nens+1)
+  end do
+
+  !inflation
+  if(inflate_adapt) inflate_coef=adapt_inflate_coef(yo,yb,ob_err**2)
+  u=u*inflate_coef
 
   !adaptively find optimal roi
   if(localize_opt==2) then
     if(proc_id==0) print *,'adaptive localization:'
     if(find_roi==1) &
-      call find_optimal_roi_mac(nens,u,kr,roi)
+      call find_optimal_roi_mac(nens,u,u,kr,roi)
     if(find_roi==2) &
       call find_optimal_roi_chi(nens,u,kr,roi)
   end if
 
   !assimilation loop
-  nobs=0
   do k=1,1 
   do j=1,ny,ob_thin
   do i=1,nx,ob_thin
     p=(k-1)*nx*ny+(j-1)*ny+i
 
     do v=1,nv !variable loop
-      nobs=nobs+1
 
       select case (obt(v))
         case (1)
@@ -138,40 +233,49 @@ program enkf
         case (2) 
           obs_val=obs_v(p) !v
         case (3) 
-          obs_val=obs_psi(p) !psi converted from uv
+          obs_val=obs_psi(p) !psi
         case (4) 
-          obs_val=obs_zeta(p) !zeta converted from uv
+          obs_val=obs_zeta(p) !zeta
         case (5) 
-          obs_val=obs_psi1(p) !psi equiv gauss noise
-        case (6) 
-          obs_val=obs_zeta1(p) !zeta equiv gauss noise
+          obs_val=obs_temp(p) !temp
       end select
 
       !observation prior
-      call grid2d((/(i,i=-kmax,kmax)/),(/(j,j=0,kmax)/),kx,ky)
       hu=0.0
       do m=1,nm
         ie=(m-1)*nprocs+proc_id+1
         if(ie<=nens) then
-          uobs=u(:,:,:,m)+um
+          uobs(:,:,:,m)=u(:,:,:,m)+um
           do k1=1,1  !no interp in vertical
-            spec=dcmplx(uobs(:,:,k1),0.0)
+            spec=dcmplx(uobs(:,:,k1,m),0.0)
             spec=fft2(spec)/(nx*ny)
             spec=fftshift(spec)
             hspec=halfspec(spec)
+            select case (state_type)
+              case (1) !u
+                hspec=hspec/dcmplx(0.0,-ky)
+              case (2) !v
+                hspec=hspec/dcmplx(0.0,kx)
+              case (4) !zeta
+                hspec=hspec/dcmplx(-(kx**2+ky**2),0.0)
+              case (5) !temp
+                hspec=hspec/dcmplx(-sqrt(kx**2+ky**2),0.0)
+            end select
             select case (obt(v))
-              case (1) 
-                hspec=dcmplx(0.0,-ky)*hspec !u
-              case (2) 
-                hspec=dcmplx(0.0,kx)*hspec !v
-              case (4,6) 
-                hspec=-dcmplx(kx**2+ky**2,0.0)*hspec !zeta
+               case (1)
+                 hspec=hspec*dcmplx(0.0,-ky)
+              case (2)
+                 hspec=hspec*dcmplx(0.0,kx)
+              case (4)
+                hspec=hspec*dcmplx(-(kx**2+ky**2),0.0)
+              case (5)
+                hspec=hspec*dcmplx(-sqrt(kx**2+ky**2),0.0)
             end select
             spec=fullspec(hspec)
             spec=ifftshift(spec)
-            uobs(:,:,k1)=real(ifft2(spec))
+            uobs(:,:,k1,m)=real(ifft2(spec))
           end do
-          call obs_interp(x,y,z,uobs,obs_x(p),obs_y(p),obs_z(p),hu(ie))
+          call obs_interp(x,y,z,uobs(:,:,:,m),obs_x(p),obs_y(p),obs_z(p),hu(ie))
         end if
       end do
       call MPI_Allreduce(hu,hu,nens+1,MPI_REAL8,MPI_SUM,comm,ierr)
@@ -181,20 +285,20 @@ program enkf
       end do
 
       innov=obs_val-hu(nens+1)
-      !if(abs(innov)>(1*ob_err)) then
-        !if(proc_id==0) print *,'kick off'
-        !cycle
-      !end if
+      if(abs(innov)>(5*ob_err)) then
+        if(proc_id==0) print *,'kick off'
+        cycle
+      end if
 
       varb=sum(hu(1:nens)**2)/real(nens-1)
       varo=ob_err**2
-      !varo=innov**2 !Huber norm or AOEI
+      if(use_aoei) varo=innov**2 !Huber norm or AOEI
       norm=varo+varb
       alpha=1.0/(1.0+sqrt(varo/norm))
 
-      !distance between obs and x
-      dist=sqrt((min(abs(x-obs_x(p)),abs(nx-x+obs_x(p))))**2 + &
-                (min(abs(y-obs_y(p)),abs(ny-y+obs_y(p))))**2)
+      !distance between obs and x, accouting for period bc
+      dist=sqrt((min(abs(x-obs_x(p)),nx-abs(x-obs_x(p))))**2 + &
+                (min(abs(y-obs_y(p)),ny-abs(y-obs_y(p))))**2)
     
       !scale separation
       u_ms=0.0
@@ -238,7 +342,7 @@ program enkf
       um=sum(um_ms,4)
 
       if(debug .and. proc_id==0) &
-        write(*,'(a,i7,i3,a,f6.2,a,f6.2,a,f5.2,a,f7.2,f7.2)') 'No',p, obt(v), &
+        write(*,'(a,i8,i3,a,f6.2,a,f6.2,a,f5.2,a,f7.2,f7.2)') 'No',p, obt(v), &
            ' (',obs_x(p),',',obs_y(p),',',obs_z(p),') ',obs_val,hu(nens+1)
 
     end do !variable loop
@@ -247,53 +351,43 @@ program enkf
   end do
   end do !assimilation loop
 
-  usave(:,:,:,:,2)=u; umsave(:,:,:,2)=um !posterior perturbation and mean
-
-  !covariance relaxation and add mean back
-  !adaptive algorithm needs O A and B in obs space  
-  call grid2d((/(i,i=-kmax,kmax)/),(/(j,j=0,kmax)/),kx,ky)
-  allocate(obsv(nrec),usavev(nx,ny,nz,nm,2),yo(nobs),ysave(nobs,nens+1,2))
+  !posterior statistics in obs space
   n=0
-  ysave=0.0
   do v=1,nv
-    select case (obt(v))
-      case (1) !u
-        obsv=obs_u
-      case (2) !v
-        obsv=obs_v
-      case (3) !psi converted from uv
-        obsv=obs_psi
-      case (4) !zeta converted from uv
-        obsv=obs_zeta
-      case (5) !psi equiv gauss noise
-        obsv=obs_psi1
-      case (6) !zeta equiv gauss noise
-        obsv=obs_zeta1
-    end select
-    do r=1,2  !prior and posterior
-      do m=1,nm
-        ie=(m-1)*nprocs+proc_id+1
-        if(ie<=nens) then
-          uobs=usave(:,:,:,m,r)+umsave(:,:,:,r)
-          do k=1,nz
-            spec=dcmplx(uobs(:,:,k),0.0)
-            spec=fft2(spec)/(nx*ny)
-            spec=fftshift(spec)
-            hspec=halfspec(spec)
-            select case (obt(v))
+    do m=1,nm
+      ie=(m-1)*nprocs+proc_id+1
+      if(ie<=nens) then
+        uobs(:,:,:,m)=u(:,:,:,m)+um
+        do k=1,nz
+          spec=dcmplx(uobs(:,:,k,m),0.0)
+          spec=fft2(spec)/(nx*ny)
+          spec=fftshift(spec)
+          hspec=halfspec(spec)
+          select case (state_type)
             case (1) !u
-              hspec=dcmplx(0.0,-ky)*hspec
+              hspec=hspec/dcmplx(0.0,-ky)
             case (2) !v
-              hspec=dcmplx(0.0,kx)*hspec
-            case (4,6) !zeta
-              hspec=-dcmplx(kx**2+ky**2,0.0)*hspec
-            end select
-            spec=fullspec(hspec)
-            spec=ifftshift(spec)
-            usavev(:,:,k,m,r)=real(ifft2(spec))
-          end do
-        end if
-      end do
+              hspec=hspec/dcmplx(0.0,kx)
+            case (4) !zeta
+              hspec=hspec/dcmplx(-(kx**2+ky**2),0.0)
+            case (5) !temp
+              hspec=hspec/dcmplx(-sqrt(kx**2+ky**2),0.0)
+          end select
+          select case (obt(v))
+            case (1)
+              hspec=hspec*dcmplx(0.0,-ky)
+            case (2)
+              hspec=hspec*dcmplx(0.0,kx)
+            case (4)
+              hspec=hspec*dcmplx(-(kx**2+ky**2),0.0)
+            case (5)
+              hspec=hspec*dcmplx(-sqrt(kx**2+ky**2),0.0)
+          end select
+          spec=fullspec(hspec)
+          spec=ifftshift(spec)
+          uobs(:,:,k,m)=real(ifft2(spec))
+        end do
+      end if
     end do
 
     do k=1,1 
@@ -301,26 +395,23 @@ program enkf
     do i=1,nx,ob_thin
       n=n+1
       p=(k-1)*nx*ny+(j-1)*ny+i
-      yo(n)=obsv(p)
-      do r=1,2  !prior and posterior
-        do m=1,nm
-          ie=(m-1)*nprocs+proc_id+1
-          if(ie<=nens) &
-            call obs_interp(x,y,z,usavev(:,:,:,m,r),obs_x(p),obs_y(p),obs_z(p),ysave(n,ie,r))
-        end do
+      do m=1,nm
+        ie=(m-1)*nprocs+proc_id+1
+        if(ie<=nens) &
+          call obs_interp(x,y,z,uobs(:,:,:,m),obs_x(p),obs_y(p),obs_z(p),ya(n,ie))
       end do
     end do
     end do
     end do
   end do
-  call MPI_Allreduce(ysave,ysave,nobs*(nens+1)*2,MPI_REAL8,MPI_SUM,comm,ierr)
-  ysave(:,nens+1,:)=sum(ysave(:,1:nens,:),2)/real(nens)
+  call MPI_Allreduce(ya,ya,nobs*(nens+1),MPI_REAL8,MPI_SUM,comm,ierr)
+  ya(:,nens+1)=sum(ya(:,1:nens),2)/real(nens)
   do m=1,nens
-    ysave(:,m,:)=ysave(:,m,:)-ysave(:,nens+1,:)
+    ya(:,m)=ya(:,m)-ya(:,nens+1)
   end do
 
-  if(relax_adapt) relax_coef=adapt_relax_coef(yo,ysave,varo)
-
+  !covariance relaxation and add mean back
+  if(relax_adapt) relax_coef=adapt_relax_coef(yo,yb,ya,varo)
   select case (relax_opt)
     case (0) !no relaxation
       do m=1,nm
@@ -328,20 +419,18 @@ program enkf
         if(ie<=nens) &
           u(:,:,:,m)=u(:,:,:,m)+um
       end do
-
     case (1) !RTPP
       if(proc_id==0) print *,'RTPP relax_coef = ',relax_coef
       do m=1,nm
         ie=(m-1)*nprocs+proc_id+1
         if(ie<=nens) &
-          u(:,:,:,m)=relax_coef*usave(:,:,:,m,1)+(1-relax_coef)*usave(:,:,:,m,2)+um
+          u(:,:,:,m)=relax_coef*uf(:,:,:,m)+(1-relax_coef)*u(:,:,:,m)+um
       end do
-
     case (2) !RTPS
       sigf=0.0
       sig=0.0
-      call MPI_Allreduce(sum(usave(:,:,:,:,1)**2,4),sigf,nx*ny*nz,MPI_REAL8,MPI_SUM,comm,ierr)
-      call MPI_Allreduce(sum(usave(:,:,:,:,2)**2,4),sig,nx*ny*nz,MPI_REAL8,MPI_SUM,comm,ierr)
+      call MPI_Allreduce(sum(uf**2,4),sigf,nx*ny*nz,MPI_REAL8,MPI_SUM,comm,ierr)
+      call MPI_Allreduce(sum(u**2,4),sig,nx*ny*nz,MPI_REAL8,MPI_SUM,comm,ierr)
       sigf=sqrt(sigf/real(nens-1))
       sig=sqrt(sig/real(nens-1))
       if(proc_id==0) print *,'RTPS relax_coef = ',relax_coef
@@ -350,7 +439,6 @@ program enkf
         if(ie<=nens) &
           u(:,:,:,m)=(relax_coef*(sigf/sig-1)+1)*u(:,:,:,m)+um
       end do
-
   end select
 
   !write out posteriors
@@ -361,8 +449,18 @@ program enkf
       do k=1,nz
         spec=dcmplx(u(:,:,k,m),0.0)
         spec=fft2(spec)/(nx*ny)
-        spec=fftshift(spec)
-        uspec(:,:,k)=cmplx(halfspec(spec))
+        hspec=halfspec(fftshift(spec))
+        select case (state_type) !convert state variable back to psi
+          case (1) !u
+            hspec=hspec/dcmplx(0.0,-ky)
+          case (2) !v
+            hspec=hspec/dcmplx(0.0,kx)
+          case (4) !zeta
+            hspec=hspec/dcmplx(-(kx**2+ky**2),0.0)
+          case (5) !temp
+            hspec=hspec/dcmplx(-sqrt(kx**2+ky**2),0.0)
+        end select
+        uspec(:,:,k)=cmplx(hspec)
       end do
       call write_field(outfile,nkx,nky,nz,uspec)
     end if
@@ -370,7 +468,7 @@ program enkf
 
   deallocate(x,y,z,kx,ky,dist,loc,cova)
   deallocate(uspec,spec,hspec,sig,sigf)
-  deallocate(u,um,usave,umsave,ysave,u_ms,um_ms,hu)
+  deallocate(u,um,uf,umf,u_ms,um_ms,hu)
 
   if(proc_id==0) print*,'enkf complete'
   call parallel_finish()
